1cec828fe4c977679dbc9d6fee89baa87592a67d
shathakh <shathaqudaih17@gmail.com>
2022-09-07 10:36:30 +0300

    create 404 and 500 errors pages relates #9

--- server/controllers/error.js
+++ server/controllers/error.js
@@ -1,12 +1,14 @@
+const {join} = require('path')
+
 const handleClientError = (req, res) => {
-  res.status(404).json('Not Found');
+  res.status(404).sendFile(join(__dirname,'..','..','public','html','404.html'))
 };
 
 const handleServerError = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).json({ msg: err.msg, status: err.status });
   } else {
-    res.status(500).json('Server Error');
+    res.status(500).sendFile(join(__dirname,'..','..','public','html','500.html'))
   }
 };
 
